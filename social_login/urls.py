5a50fceb9d1147c3e513f13fb4fd0fb6eafc89fd
Nikolai <konovalov.nikolai@gmail.com>
2013-10-28 18:21:00 +0600

    sdf

--- social_login/urls.py
+++ social_login/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('social_login.views',
    url(r'^vk/$', 'vk_auth', name='vk'),
-   url(r'^ok/$', 'ok_auth', name='ok'),
+   url(r'^ok/$', 'odnokl_auth', name='odnokl'),
    url(r'^fb/$', 'fb_auth', name='fb'),
    )
